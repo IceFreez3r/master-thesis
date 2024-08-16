619078ccca25e27e813e063e5efe20412cae15f7
IceFreez3r <39794212+IceFreez3r@users.noreply.github.com>
2024-08-16 17:24:14 +0200

    Smarter region extraction

--- reduce_to_region.py
+++ reduce_to_region.py
@@ -16,17 +16,23 @@ def main(region, gtf, bams, output):
         os.makedirs(output)
 
     reduce_gtf(gtf, chr, start, end, output)
+    output_bams = []
     for bam in bams:
-        reduce_bam(bam, chr, start, end, output)
+        output_bams.append(reduce_bam(bam, chr, start, end, output))
+
+    with open(os.path.join(output, f'metadata_{chr}_{start}_{end}.tsv'), 'w') as f:
+        f.write('sample ID\tfile\n')
+        for bam in output_bams:
+            f.write(f'{'\t'.join(bam)}\n')
 
 def reduce_gtf(gtf, chr, start, end, output):
     print(f'Reducing GTF file {gtf} to region {chr}:{start}-{end}')
     # reduce gtf file to region and output to output directory
     ext = os.path.splitext(gtf)[1]
     if ext == '.gz':
-        cmd = f'zcat {gtf} | awk \'$1 == "{chr}" && $4 >= {start} && $5 <= {end}\' | gzip > {output}/reduced_{chr}_{start}_{end}.gtf.gz'
+        cmd = f'zcat {gtf} | awk \'$1 == "{chr}" && $4 >= {start} && $5 <= {end}\' | gzip > {os.path.join(output, f'reduced_{chr}_{start}_{end}.gtf.gz')}'
     elif ext == '.gtf':
-        cmd = f'awk \'$1 == "{chr}" && $4 >= {start} && $5 <= {end}\' {gtf} > {output}/reduced_{chr}_{start}_{end}.gtf'
+        cmd = f'awk \'$1 == "{chr}" && $4 >= {start} && $5 <= {end}\' {gtf} > {os.path.join(output, f'reduced_{chr}_{start}_{end}.gtf')}'
     print(cmd)
     subprocess.run(cmd, shell=True)
 
@@ -36,9 +42,14 @@ def reduce_bam(bam, chr, start, end, output):
     # reduce bam file to region and output to output directory
     filename, ext = os.path.splitext(os.path.basename(bam))
     assert ext == '.bam', 'Input file must be a BAM file'
-    cmd = f'samtools view -b {bam} {chr}:{start}-{end} > {output}/{filename}_{chr}_{start}_{end}.bam'
+    output_file = os.path.join(output, f'{filename}_{chr}_{start}_{end}.bam')
+    cmd = f'samtools view -b {bam} {chr}:{start}-{end} > {output_file}'
     print(cmd)
     subprocess.run(cmd, shell=True)
+    cmd = f'samtools index {output_file}'
+    print(cmd)
+    subprocess.run(cmd, shell=True)
+    return filename, os.path.abspath(output_file)
 
 if __name__ == '__main__':
     main()
